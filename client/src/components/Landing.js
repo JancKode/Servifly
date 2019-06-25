55b4c77df6067acd240eaa86995e218798e88396
Gynnaux <rso.janc@gmail.com>
2019-06-25 14:06:16 +0800

    Additional changes

---
+++ client/src/components/Landing.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+const Landing = () => {
+  return (
+    <div className="row">
+      <div className="row s10 m4 center">
+        <div className="card black darken-1">
+          <div className="card-content blue">
+            <span className="card-title white-text">Servifly</span>
+            <p className="white-text">Gather feedback from your users</p>
+          </div>
+          <div className="card-action ">
+            <a href="#">Inquire</a>
+            <a href="#">Let's get started!</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
