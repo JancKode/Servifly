85c83a5c7f044a6e52473b4926b1afe799991f88
Gynnaux <rso.janc@gmail.com>
2019-06-25 09:23:43 +0800

    Added React UI

--- services/passport.js
+++ services/passport.js
@@ -23,7 +23,7 @@ passport.use(
       callbackURL: "/auth/google/callback",
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
+    /*(accessToken, refreshToken, profile, done) => {
       User.findOne({ googleId: profile.id }).then(existingUser => {
         if (existingUser) {
           //record exists
@@ -34,6 +34,16 @@ passport.use(
             .then(user => done(null, user));
         }
       });
+    }*/
+
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+
+      if (existingUser) {
+        return done(null, existingUser);
+      }
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
     }
   )
 );
